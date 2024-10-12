88a5ae32d3371c95021ea8d2011f6d4de64ce978
Sylv <contact@sylv.gay>
2024-10-12 17:21:09 -0400

    fix: optimize client-side tint updates

--- src/main/java/gay/sylv/legacy_landscape/networking/ClientPayloadHandler.java
+++ src/main/java/gay/sylv/legacy_landscape/networking/ClientPayloadHandler.java
@@ -3,7 +3,8 @@ package gay.sylv.legacy_landscape.networking;
 import gay.sylv.legacy_landscape.data_attachment.LegacyAttachments;
 import gay.sylv.legacy_landscape.mixin.ClientLevelAccessor;
 import gay.sylv.legacy_landscape.networking.client_bound.LegacyChunkPayload;
-import net.minecraft.world.level.Level;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
@@ -13,14 +14,17 @@ public final class ClientPayloadHandler {
 	private ClientPayloadHandler() {}
 
 	public static void handleLegacyChunkPayload(final LegacyChunkPayload payload, final IPayloadContext context) {
-		try (Level level = context.player().level()) {
+		try (ClientLevel level = (ClientLevel) context.player().level()) {
 			LevelChunk chunk = level.getChunk(payload.chunkPos().x, payload.chunkPos().z);
-			((ClientLevelAccessor) level).getTintCaches().forEach((colorResolver, blockTintCache) -> blockTintCache.invalidateForChunk(chunk.getPos().x, chunk.getPos().z));
 			LegacyNetworking.LOGGER.info("{}", payload);
 			if (payload.isLegacyChunk()) {
 				chunk.setData(LegacyAttachments.LEGACY_CHUNK, true);
 			} else {
 				chunk.removeData(LegacyAttachments.LEGACY_CHUNK);
+			}
+			((ClientLevelAccessor) level).getTintCaches().forEach((colorResolver, blockTintCache) -> blockTintCache.invalidateForChunk(chunk.getPos().x, chunk.getPos().z));
+			for (int y = level.getMinSection(); y < level.getMaxSection(); y++) {
+				Minecraft.getInstance().levelRenderer.setSectionDirty(chunk.getPos().x, y, chunk.getPos().z);
 			}
 		} catch (IOException e) {
 			LegacyNetworking.LOGGER.error("Failed to handle legacy chunk payload", e);
