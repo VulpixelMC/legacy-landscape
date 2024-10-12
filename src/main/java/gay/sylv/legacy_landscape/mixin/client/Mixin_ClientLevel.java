c63b097cf40fbedaa0d70d50962e9f6296b7bccb
Sylv <contact@sylv.gay>
2024-10-12 18:22:54 -0400

    tweak: brighten foliage, revert water color to pre-1.13

--- src/main/java/gay/sylv/legacy_landscape/mixin/client/Mixin_ClientLevel.java
+++ src/main/java/gay/sylv/legacy_landscape/mixin/client/Mixin_ClientLevel.java
@@ -4,25 +4,18 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import gay.sylv.legacy_landscape.data_attachment.LegacyAttachments;
-import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
 import net.minecraft.client.OptionInstance;
-import net.minecraft.client.color.block.BlockTintCache;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.ColorResolver;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.chunk.LevelChunk;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ClientLevel.class)
 public final class Mixin_ClientLevel {
-	@Shadow
-	@Final
-	private Object2ObjectArrayMap<ColorResolver, BlockTintCache> tintCaches;
-
 	private Mixin_ClientLevel() {}
 
 	@WrapOperation(
@@ -52,11 +45,21 @@ public final class Mixin_ClientLevel {
 	private int saturateBlockTint(ColorResolver colorResolver, Biome biome, double x, double z, Operation<Integer> original, @Local(argsOnly = true) BlockPos blockPos) {
 		var self = (ClientLevel) (Object) this;
 		LevelChunk chunk = self.getChunkAt(blockPos);
-		int tint = original.call(colorResolver, biome, x, z);
 		if (chunk.getData(LegacyAttachments.LEGACY_CHUNK)) {
-			int humidity = ((tint >> 8) + 0x8) ^ 0xFFFF00FF;
-			tint = (tint ^ 0xFF00) | humidity; // clear green channel and set new humidity
+			if (!colorResolver.equals(BiomeColors.WATER_COLOR_RESOLVER)) {
+				int tint = original.call(colorResolver, biome, x, z);
+				int red = ((tint & 0x00FF0000)) >> 16;
+				red += 0x28;
+				red = Math.clamp(red, 0, 0xFF);
+				int green = ((tint & 0x0000FF00)) >> 8;
+				green += 0x28;
+				green = Math.clamp(green, 0, 0xFF); // prevent overflow/underflow
+				tint = (tint & 0xFF0000FF) | green << 8 | red << 16; // clear green channel and set new green
+				return tint;
+			} else {
+				return 0x334FDD;
+			}
 		}
-		return tint;
+		return original.call(colorResolver, biome, x, z);
 	}
 }
