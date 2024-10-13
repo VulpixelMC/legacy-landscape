fe76c527e1b474d1ae99cdb1f2c7178a73331c3f
Sylv <contact@sylv.gay>
2024-10-13 15:57:25 -0400

    fix(day 3): add @Pseudo to Mixin_LevelColorCache

--- src/main/java/gay/sylv/legacy_landscape/mixin/client/sodium/Mixin_LevelColorCache.java
+++ src/main/java/gay/sylv/legacy_landscape/mixin/client/sodium/Mixin_LevelColorCache.java
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelColorCache.class)
+@Pseudo
 public final class Mixin_LevelColorCache {
 	@Mutable
 	@Shadow
