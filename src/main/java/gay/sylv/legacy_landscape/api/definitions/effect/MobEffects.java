a03407f60d07aa68a862b41da9ea2df3e177183c
sylv256 <contact@sylv.gay>
2024-11-13 16:34:31 -0500

    feat(api): public-facing MobEffects class

---
+++ src/main/java/gay/sylv/legacy_landscape/api/definitions/effect/MobEffects.java
@@ -0,0 +1,10 @@
+package gay.sylv.legacy_landscape.api.definitions.effect;
+
+import gay.sylv.legacy_landscape.effect.LegacyEffects;
+import net.minecraft.world.effect.MobEffect;
+
+public final class MobEffects {
+	public static MobEffect evanescence() {
+		return LegacyEffects.EVANESCENCE.get();
+	}
+}
